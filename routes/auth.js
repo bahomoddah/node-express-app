05e8c505c7b70b245090b28a7405cf1b4c1fea42
bahomoddah <bahomoddah@gmail.com>
2022-08-11 14:23:35 +0300

    added validation for signup

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,28 @@ var router = express.Router();
 
 // get Signup page
 router.get('/signup', function(req, res, next) {
-    res.render('signup', { title: 'Validation', success: false, errors: null});
+    res.render('signup', { title: 'Validation', success: req.session.seccess , errors: req.session.errors});
+    req.session.errors = null
+    req.session.success = null
 });
 
 // SignUp POST
 router.post('/register', function(req, res, next) {
     console.log('You signed up');
-    res.redirect('/')
+    req.check('email', 'Invalid email address').isEmail()
+    req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors
+        req.session.seccess = false
+        res.redirect('/auth/signup')
+    } else {
+        req.session.seccess = true
+        res.redirect('/auth/signup')
+
+    }
+    // res.redirect('/')
 })
 
 module.exports = router;
