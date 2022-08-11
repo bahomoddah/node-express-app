04d887dd91fdb712ba7a45bed85e9a2fce4a8461
bahomoddah <bahomoddah@gmail.com>
2022-08-11 14:07:57 +0300

    design signup page with its route get and post

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 // get Signup page
 router.get('/signup', function(req, res, next) {
-    res.render('signup');
+    res.render('signup', { title: 'Validation', success: false, errors: null});
 });
+
+// SignUp POST
+router.post('/register', function(req, res, next) {
+    console.log('You signed up');
+    res.redirect('/')
+})
 
 module.exports = router;
