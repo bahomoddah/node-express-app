6388ca761157865c3666d084bc68e672706ecb99
bahomoddah <bahomoddah@gmail.com>
2022-08-11 11:21:05 +0300

    create route for all route you need

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', author: 'Bahomoddah', names: ['Ali', 'Ahmed'] });
 });
 
+// Get car details
+router.get('/car/:id', function(req, res, next) {
+  res.render('car', { output: req.params.id })
+})
+
+router.get('/cars', function(req, res, next) {
+  res.render('cars')
+})
+
+// POST new car
+router.post('/cars/add', function(req, res, next) {
+  var id = req.body.id
+  res.redirect('/car/' + id)
+})
+
 module.exports = router;
