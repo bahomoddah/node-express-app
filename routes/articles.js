02104f0ab9bd1711a46e4a7d2ac51376322b6335
bahomoddah <bahomoddah@gmail.com>
2022-08-14 17:53:23 +0300

    get and add new article from locale mongoDB

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
 
-/* GET Articles */
+const url = "mongodb://localhost:27017/myExpressDB"
+/* GET Articles Page */
 router.get('/', function(req, res, next) {
   res.render('articles');
 });
 
+/* GET Articles Data*/
+router.get('/get', function(req, res, next) {
+  var arr = []
+
+    mongo.connect(url, (err, client) => {
+        assert.equal(null, err);
+        var db = client.db();
+        var articlesData = db.collection('articles').find();
+        console.log('articles', articlesData);
+        articlesData.forEach((doc, err) => {
+            assert.equal(null, err);
+            arr.push(doc)
+        },
+        () => {
+            client.close()
+            res.render('articles', { articles: arr })
+        })
+    })
+  });
+
 // Added New Article
 router.post('/add', function(req, res, next) {
+    var article = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    };
+
+    mongo.connect(url, function(err, client) {
+        var db = client.db();
+        assert.equal(null, err);
+        db.collection('articles').insertOne(article, function(err, result) {
+            assert.equal(null, err);
+            console.log("Article Added");
+            client.close();
+        });
+    })
+
+    res.redirect('/articles')
 })
 
 // update Article
