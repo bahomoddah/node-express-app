cf80bd77125604f8251263a42e64fb20d13b5753
bahomoddah <bahomoddah@gmail.com>
2022-08-20 18:12:04 +0300

    create User (model) from mongoose with its (Schema) to use it withmongoose  (query)

---
+++ mongoose/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const addressSchema = mongoose.Schema({
+    street: String,
+    city: String
+})
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        minLength: 3
+    },
+    lastName: String,
+    email: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    age: {
+        type: Number,
+        min: 13,
+        max: 77,
+        default: 25
+    },
+    createAt: {
+        type: Date
+    },
+    updateAt: {
+        type: Date
+    },
+    address: addressSchema,
+    homeTwon:  {
+        country: String,
+        city: String
+    },
+    hoppies: []
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
